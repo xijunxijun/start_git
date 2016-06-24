75120acf07d6741865b8e07d38ecb9e718237e82
xj <1017053689@qq.com>
2016-06-24 15:01:45 +0800

    fix merge 2

--- code.py
+++ code.py
@@ -11,8 +11,5 @@ print "git  checkout me after add or commit is ok"
 print "git  checkout HEAD me is come back from repository and overwrite working area and buffer cache"
 print "git branch name; git checkout name    ==  git checkout -b name"
 print "master change first.than branch change"
-<<<<<<< HEAD
 print "master change with branch "
-=======
 print "branch not change by master; now branch change going"
->>>>>>> idea_one
